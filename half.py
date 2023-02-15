1c8af255b9a241818fa3770b9ee127eba1f91e25
ADICODITAS <123449022+ADICODITAS@users.noreply.github.com>
2023-02-15 13:36:19 +0530

    Update half.py

--- half.py
+++ half.py
@@ -1,3 +1,7 @@
+#code for file ,we have to make a program  in python that will take a file path as input and a delimiter what we will we using in the file to separate the numbers (integers),after that seprate the number from the file and 
+# add all the numbers that were present in the file where will only one type of delimiter in the file    
+
+
 deli=input("enter delimiter that you useed in the file")
 filee=input("enter file path")
 def sum_numbers_in_file(deli):
